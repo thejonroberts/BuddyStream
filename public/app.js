9888872e65ba2c146385c0b31b0d3448f7c7bae3
thejonroberts <jon.roberts.8@gmail.com>
2017-11-04 20:15:13 -0500

    server working, starts websocket and pulls in remote stream

--- public/app.js
+++ public/app.js
@@ -1,44 +1,181 @@
 /*  FROM WS npm module example: global fetch, WebSocket, location */
-const messages = document.querySelector('#messages');
-const wsButton = document.querySelector('#wsButton');
-const logout = document.querySelector('#logout');
-const login = document.querySelector('#login');
+// const messages = document.querySelector('#messages');
+// const wsButton = document.querySelector('#wsButton');
+// const logout = document.querySelector('#logout');
+// const login = document.querySelector('#login');
 
-const showMessage = message => {
-	messages.textContent += `\n${message}`;
-	messages.scrollTop = messages.scrollHeight;
+// const showMessage = message => {
+// 	messages.textContent += `\n${message}`;
+// 	messages.scrollTop = messages.scrollHeight;
+// };
+
+// const handleResponse = response => {
+// 	return response.ok
+// 		? response.json().then(data => JSON.stringify(data, null, 2))
+// 		: Promise.reject(new Error('Unexpected response'));
+// };
+
+// login.onclick = () => {
+// 	fetch('/login', { method: 'POST', credentials: 'same-origin' })
+// 		.then(handleResponse)
+// 		.then(showMessage)
+// 		.catch(err => showMessage(err.message));
+// };
+
+// logout.onclick = () => {
+// 	fetch('/logout', { method: 'DELETE', credentials: 'same-origin' })
+// 		.then(handleResponse)
+// 		.then(showMessage)
+// 		.catch(err => showMessage(err.message));
+// };
+
+// let ws;
+
+// wsButton.onclick = () => {
+// 	if (ws) {
+// 		ws.onerror = ws.onopen = ws.onclose = null;
+// 		ws.close();
+// 	}
+
+// 	ws = new WebSocket(`ws://${location.host}`);
+// 	ws.onerror = () => showMessage('WebSocket error');
+// 	ws.onopen = () => showMessage('WebSocket connection established');
+// 	ws.onclose = () => showMessage('WebSocket connection closed');
+// };
+
+var localVideo;
+var remoteVideo;
+var peerConnection;
+var uuid;
+
+var peerConnectionConfig = {
+	iceServers: [
+		{ urls: 'stun:stun.services.mozilla.com' },
+		{ urls: 'stun:stun.l.google.com:19302' }
+	]
 };
 
-const handleResponse = response => {
-	return response.ok
-		? response.json().then(data => JSON.stringify(data, null, 2))
-		: Promise.reject(new Error('Unexpected response'));
-};
+function pageReady() {
+	uuid = uuid();
 
-login.onclick = () => {
-	fetch('/login', { method: 'POST', credentials: 'same-origin' })
-		.then(handleResponse)
-		.then(showMessage)
-		.catch(err => showMessage(err.message));
-};
+	localVideo = document.getElementById('localVideo');
+	remoteVideo = document.getElementById('remoteVideo');
 
-logout.onclick = () => {
-	fetch('/logout', { method: 'DELETE', credentials: 'same-origin' })
-		.then(handleResponse)
-		.then(showMessage)
-		.catch(err => showMessage(err.message));
-};
+	let PORT = 8443;
+	serverConnection = new WebSocket(`wss://${window.location.hostname}:${PORT}`);
+	serverConnection.onmessage = gotMessageFromServer;
 
-let ws;
+	var constraints = {
+		video: true,
+		audio: true
+	};
 
-wsButton.onclick = () => {
-	if (ws) {
-		ws.onerror = ws.onopen = ws.onclose = null;
-		ws.close();
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices
+			.getUserMedia(constraints)
+			.then(getUserMediaSuccess)
+			.catch(errorHandler);
+	} else {
+		alert('Your browser does not support getUserMedia API');
+	}
+}
+
+function getUserMediaSuccess(stream) {
+	localStream = stream;
+	localVideo.src = window.URL.createObjectURL(stream);
+}
+
+function start(isCaller) {
+	peerConnection = new RTCPeerConnection(peerConnectionConfig);
+	peerConnection.onicecandidate = gotIceCandidate;
+	peerConnection.onaddstream = gotRemoteStream;
+	peerConnection.addStream(localStream);
+
+	if (isCaller) {
+		peerConnection
+			.createOffer()
+			.then(createdDescription)
+			.catch(errorHandler);
+	}
+}
+
+function gotMessageFromServer(message) {
+	if (!peerConnection) start(false);
+
+	var signal = JSON.parse(message.data);
+
+	// Ignore messages from ourself
+	if (signal.uuid == uuid) return;
+
+	if (signal.sdp) {
+		peerConnection
+			.setRemoteDescription(new RTCSessionDescription(signal.sdp))
+			.then(function() {
+				// Only create answers in response to offers
+				if (signal.sdp.type == 'offer') {
+					peerConnection
+						.createAnswer()
+						.then(createdDescription)
+						.catch(errorHandler);
+				}
+			})
+			.catch(errorHandler);
+	} else if (signal.ice) {
+		peerConnection
+			.addIceCandidate(new RTCIceCandidate(signal.ice))
+			.catch(errorHandler);
+	}
+}
+
+function gotIceCandidate(event) {
+	if (event.candidate != null) {
+		serverConnection.send(JSON.stringify({ ice: event.candidate, uuid: uuid }));
+	}
+}
+
+function createdDescription(description) {
+	console.log('got description');
+
+	peerConnection
+		.setLocalDescription(description)
+		.then(function() {
+			serverConnection.send(
+				JSON.stringify({ sdp: peerConnection.localDescription, uuid: uuid })
+			);
+		})
+		.catch(errorHandler);
+}
+
+function gotRemoteStream(event) {
+	console.log('got remote stream');
+	remoteVideo.src = window.URL.createObjectURL(event.stream);
+}
+
+function errorHandler(error) {
+	console.log(error);
+}
+
+// Taken from http://stackoverflow.com/a/105074/515584
+// Strictly speaking, it's not a real UUID, but it gets the job done here
+function uuid() {
+	function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
 	}
 
-	ws = new WebSocket(`ws://${location.host}`);
-	ws.onerror = () => showMessage('WebSocket error');
-	ws.onopen = () => showMessage('WebSocket connection established');
-	ws.onclose = () => showMessage('WebSocket connection closed');
-};
+	return (
+		s4() +
+		s4() +
+		'-' +
+		s4() +
+		'-' +
+		s4() +
+		'-' +
+		s4() +
+		'-' +
+		s4() +
+		s4() +
+		s4()
+	);
+}
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 'use strict';
 
+// this is a hybrid of two webrtc example projects
+// TODO sing their praises in the readme!
+
+require('dotenv').config();
+const HTTPS_PORT = process.env.HTTPS_PORT;
+const PORT = 8443;
+
 const session = require('express-session');
 const express = require('express');
-const http = require('http');
 const uuid = require('uuid');
-
+const app = express();
+const fs = require('fs');
+const https = require('https');
 const WebSocket = require('ws');
 
-const app = express();
-
+const serverConfig = {
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem')
+};
 //
 // We need the same instance of the session parser in express and
 // WebSocket server.
@@ -18,61 +28,127 @@ const sessionParser = session({
 	secret: '$eCuRiTy',
 	resave: false
 });
-
 //
 // Serve static files from the 'public' folder.
 //
 app.use(express.static('public'));
 app.use(sessionParser);
 
-app.post('/login', (req, res) => {
-	//
-	// "Log in" user and set userId to session.
-	//
-	const id = uuid.v4();
+// TODO - use this to login / authenticate user
+// app.post('/login', (req, res) => {
+// 	//
+// 	// "Log in" user and set userId to session.
+// 	//
+// 	const id = uuid.v4();
 
-	console.log(`Updating session for user ${id}`);
-	req.session.userId = id;
-	res.send({ result: 'OK', message: 'Session updated' });
-});
+// 	console.log(`Updating session for user ${id}`);
+// 	req.session.userId = id;
+// 	res.send({ result: 'OK', message: 'Session updated' });
+// });
 
-app.delete('/logout', (request, response) => {
-	console.log('Destroying session');
-	request.session.destroy();
-	response.send({ result: 'OK', message: 'Session destroyed' });
-});
+// app.delete('/logout', (request, response) => {
+// 	console.log('Destroying session');
+// 	request.session.destroy();
+// 	response.send({ result: 'OK', message: 'Session destroyed' });
+// });
 
 //
 // Create HTTP server by ourselves.
 //
-const server = http.createServer(app);
+const server = https.createServer(serverConfig, app);
+//-- example
+// var httpsServer = https.createServer(serverConfig, handleRequest);
+// server.listen(HTTPS_PORT, '0.0.0.0');
 
 const wss = new WebSocket.Server({
-	verifyClient: (info, done) => {
-		console.log('Parsing session from request...');
-		sessionParser(info.req, {}, () => {
-			console.log('Session is parsed!');
-
-			//
-			// We can reject the connection by returning false to done(). For example,
-			// reject here if user is unknown.
-			//
-			done(info.req.session.userId);
-		});
-	},
+	// verifyClient: (info, done) => {
+	// 	console.log('Parsing session from request...');
+	// 	sessionParser(info.req, {}, () => {
+	// 		console.log('Session is parsed!');
+	// 		//
+	// 		// We can reject the connection by returning false to done(). For example,
+	// 		// reject here if user is unknown.
+	// 		//
+	// 		done(info.req.session.userId);
+	// 	});
+	// },
 	server
 });
 
-wss.on('connection', (ws, req) => {
-	ws.on('message', message => {
-		//
-		// Here we can now use session parameters.
-		//
-		console.log(`WS message ${message} from user ${req.session.userId}`);
+// wss.on('connection', (ws, req) => {
+// 	ws.on('message', message => {
+// 		//
+// 		// Here we can now use session parameters.
+// 		//
+// 		console.log(`WS message ${message} from user ${req.session.userId}`);
+// 	});
+// });
+
+//-SERVER STARTED IN ABOVE EXAMPLE
+
+// BEGINNING OF SIMPLE WEBRTC EXAMPLE
+// TODO move this to .env
+// const HTTPS_PORT = 8443;
+
+// const fs = require('fs');
+// const https = require('https');
+// const WebSocket = require('ws');
+// const WebSocketServer = WebSocket.Server;
+
+// Yes, SSL is required
+// const serverConfig = {
+// 	key: fs.readFileSync('key.pem'),
+// 	cert: fs.readFileSync('cert.pem')
+// };
+
+// ----------------------------------------------------------------------------------------
+
+// THIS SHOULD BE HANDLED BY EXPRESS /public routing
+// Create a server for the client html page
+// var handleRequest = function(request, response) {
+// 	// Render the single client html file for any request the HTTP server receives
+// 	console.log('request received: ' + request.url);
+
+// 	if (request.url === '/') {
+// 		response.writeHead(200, { 'Content-Type': 'text/html' });
+// 		response.end(fs.readFileSync('client/index.html'));
+// 	} else if (request.url === '/webrtc.js') {
+// 		response.writeHead(200, { 'Content-Type': 'application/javascript' });
+// 		response.end(fs.readFileSync('client/webrtc.js'));
+// 	}
+// };
+
+// var httpsServer = https.createServer(serverConfig, handleRequest);
+// httpsServer.listen(HTTPS_PORT, '0.0.0.0');
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for handling websocket calls
+// var wss = new WebSocketServer({ server: httpsServer });
+
+wss.on('connection', function(ws) {
+	ws.on('message', function(message) {
+		// Broadcast any received message to all clients
+		console.log('received: %s', message);
+		wss.broadcast(message);
 	});
 });
+
+wss.broadcast = function(data) {
+	this.clients.forEach(function(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
+};
 
 //
 // Start the server.
 //
-server.listen(8080, () => console.log('Listening on http://localhost:8080'));
+server.listen(HTTPS_PORT, () => {
+	console.log(
+		'Server running. Visit https://localhost:' +
+			HTTPS_PORT +
+			' (note the HTTPS; there is no HTTP -> HTTPS redirect!)'
+	);
+});
