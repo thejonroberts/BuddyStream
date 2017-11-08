f282dfba8db885611cd67e277bbf9dbffce1935b
thejonroberts <jon.roberts.8@gmail.com>
2017-11-08 13:37:31 -0600

    import webcam canvas effects example - before integrating

--- public/app.js
+++ public/app.js
@@ -14,24 +14,12 @@ var peerConnectionConfig = {
 	]
 };
 
+// wait for page load (iframe unreliabe load time)
 $(document).ready(() => {
 	uuid = uuid();
 
 	localVideo = document.getElementById('localVideo');
 	remoteVideo = document.getElementById('remoteVideo');
-
-	// FROM WS npm module example:
-	// wsButton.onclick = () => {
-	// 	if (ws) {
-	// 		ws.onerror = ws.onopen = ws.onclose = null;
-	// 		ws.close();
-	// 	}
-
-	// 	ws = new WebSocket(`ws://${location.host}`);
-	// };
-	// 	ws.onerror = () => showMessage('WebSocket error');
-	// 	ws.onopen = () => showMessage('WebSocket connection established');
-	// 	ws.onclose = () => showMessage('WebSocket connection closed');
 	serverConnection = new WebSocket(`wss://${window.location.hostname}:${PORT}`);
 	serverConnection.onmessage = gotMessageFromServer;
 
@@ -149,6 +137,91 @@ function uuid() {
 		s4()
 	);
 }
+
+// WEBCAM / CANVAS EFFECTS
+
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	// console.log(width, height); // 640 x 480
+	// make canvas larger than video resolution, to avoid grainy scaling when drawing to canvas.
+	canvas.width = width * 4;
+	canvas.height = height * 4;
+
+	return setInterval(() => {
+		// (src, xstart, ystart(topleft), xlength, ylength)
+		ctx.drawImage(video, 0, Math.floor(height * 2), width, height);
+		// take pixels out - need to either take whole canvas, or just video portion, and place in same place with putImageData below
+		let pixels = ctx.getImageData(0, Math.floor(height * 2), width, height);
+		// add effects - greenscreen first, then apply effect
+		pixels = greenScreen(pixels);
+		pixels = silhouetteEffect(pixels);
+		// put altered image back on canvas - has to be same coordinates from getImageData!! otherwise tiling occurs
+		ctx.putImageData(pixels, 0, Math.floor(height * 2));
+	}, 20);
+}
+
+// apply effect to webcam
+function silhouetteEffect(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i + 0] = 0; // red
+		pixels.data[i + 1] = 0; // green
+		pixels.data[i + 2] = 0; // blue
+	}
+	return pixels;
+}
+
+// take pixels that match certain RGB value to transparent (remove from video)
+function greenScreen(pixels) {
+	const levels = {};
+
+	// Select input sliders and grab values
+	document.querySelectorAll('.rgb input').forEach(input => {
+		levels[input.name] = input.value;
+	});
+
+	for (let i = 0; i < pixels.data.length; i = i + 4) {
+		let red = pixels.data[i + 0];
+		let green = pixels.data[i + 1];
+		let blue = pixels.data[i + 2];
+		// let alpha = pixels.data[i + 3];
+		// if between slider values
+		if (
+			red >= levels.rmin &&
+			red <= levels.rmax &&
+			green >= levels.gmin &&
+			green <= levels.gmax &&
+			blue >= levels.bmin &&
+			blue <= levels.bmax
+		) {
+			// take it out by setting alpha to zero
+			pixels.data[i + 3] = 0;
+		}
+	}
+	// return image with out of range pixels removed
+	return pixels;
+}
+
+// getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
+
+// FROM WS npm module example  - after declarations top of doc.ready fn
+// wsButton.onclick = () => {
+// 	if (ws) {
+// 		ws.onerror = ws.onopen = ws.onclose = null;
+// 		ws.close();
+// 	}
+
+// 	ws = new WebSocket(`ws://${location.host}`);
+// };
+// 	ws.onerror = () => showMessage('WebSocket error');
+// 	ws.onopen = () => showMessage('WebSocket connection established');
+// 	ws.onclose = () => showMessage('WebSocket connection closed');
 
 /*  FROM WS npm module example: global fetch, WebSocket, location */
 // const messages = document.querySelector('#messages');
